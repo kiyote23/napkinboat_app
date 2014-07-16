80ec51b78bd31626e1c3473c7bb9e26135fdd7e2
Peter Birk <kiyote23@gmail.com>
2014-07-15 21:51:11 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NapkinboatApp::Application.config.secret_key_base = 'f11dced64c7ee7f00ab9acbfee54f11d0ba2f1e5cc03df06c1c80ffe3d3835a0798ca5ddd09c6ad51331d4b224e0142b30ef0db48c2ce73f366cbb54a7a402a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NapkinboatApp::Application.config.session_store :cookie_store, key: '_napkinboat_app_session'

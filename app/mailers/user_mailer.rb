44fd791935a2bb20e4d5a898a38ad0c5cb934fa9
Peter Birk <kiyote23@gmail.com>
2014-07-23 00:05:39 -0500

    Got basic Mailman function working.

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,4 @@
+class UserMailer < ActionMailer::Base
+  default from: "napkinboat@dorkfarm.net"
+  	  reply_to: "napkinboat@dorkfarm.net"
+end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,5 @@
 class Message < ActiveRecord::Base
+	def self.receive_mail(message)
+		Message.create! sender_email: message.from.first, content: message.body.decoded
+	end
 end
